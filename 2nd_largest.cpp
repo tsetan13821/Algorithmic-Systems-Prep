0ac6be7bd2f9ffc4056d97357d1bdc427ecf4cae
tsetan13821 <tsetan13821@gmail.com>
2026-02-21 17:19:20 +0530

    today's progress

---
+++ 2nd_largest.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int slargest = -1;
+    int largest = arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]>largest){
+            slargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i]<largest && arr[i]>slargest){
+            slargest = arr[i];
+        }
+        else continue;
+    }
+    cout<<largest<<endl;
+    cout<<slargest<<endl;
+    return 0;
+}
